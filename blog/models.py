159ee73f7eb9083f40e2d56f86b22d74760d2031
Margulan Tukhfatov <tukhfatov.margulan@gmail.com>
2016-03-05 04:27:21 +0500

    slug field added

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.contrib.auth.models import User
+from django.template.defaultfilters import slugify
 
 # Create your models here.
 
@@ -10,4 +11,13 @@ class Post(models.Model):
 	post_text = models.TextField()
 	date_created = models.DateTimeField(auto_now_add=True)
 	author = models.ForeignKey(User)
+	post_slug = models.SlugField(max_length=60, blank=True)
+
+	def save(self, *args, **kwargs):
+		if not self.id:
+			self.post_slug = slugify(self.post_title)
+
+		super(Post, self).save(*args, **kwargs)
+
+
 
