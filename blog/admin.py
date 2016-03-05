da472fc56257f0fc80a9c30c887a908a280a4132
Margulan Tukhfatov <tukhfatov.margulan@gmail.com>
2016-03-05 05:08:58 +0500

    SlugField added, translate features implemented

--- blog/admin.py
+++ blog/admin.py
@@ -2,4 +2,5 @@ from django.contrib import admin
 from blog.models import Post
 # Register your models here.
 
+
 admin.site.register(Post)
--- blog/models.py
+++ blog/models.py
@@ -2,8 +2,8 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.contrib.auth.models import User
-from django.template.defaultfilters import slugify
-
+from pytils import translit
+import itertools
 # Create your models here.
 
 class Post(models.Model):
@@ -11,13 +11,15 @@ class Post(models.Model):
 	post_text = models.TextField()
 	date_created = models.DateTimeField(auto_now_add=True)
 	author = models.ForeignKey(User)
-	post_slug = models.SlugField(max_length=60, blank=True)
+	post_slug = models.SlugField(max_length=20, blank=True)
 
 	def save(self, *args, **kwargs):
-		if not self.id:
-			self.post_slug = slugify(self.post_title)
+		max_length = Post._meta.get_field('post_slug').max_length
+		self.post_slug = original = translit.slugify(self.post_title)[:max_length]
+
+		for x in itertools.count(1):
+			if not Post.objects.filter(post_slug=self.post_slug).exclude(pk=self.id).exists():
+				break
+			self.post_slug = '%s-%d' % (original[:max_length - len(str(x)) - 1], x)
 
 		super(Post, self).save(*args, **kwargs)
-
-
-
